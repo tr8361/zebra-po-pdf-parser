f15cc42335ce91257fc28cf3f106ab8b353a1c1c
tr8361 <tr8361@zebra.com>
2024-06-12 18:08:30 +0530

    new updtaed function_app

--- function_app.py
+++ function_app.py
@@ -1,8 +1,11 @@
+import azure.functions as func
+import logging
 import os
 import tempfile
 import io
-import azure.functions as func
-import logging
+import base64
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
 import config
 import cx_Oracle
 from datetime import datetime
@@ -18,8 +21,6 @@ from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
 
 class PurchaseOrder(BaseModel):
     po_number: str = Field(description="Purchase Order Number")
@@ -37,16 +38,17 @@ def document_load_and_parse(temp_pdf_path,prompt,client):
     loader = PyPDFLoader(temp_pdf_path)
     document = loader.load()
     print(document[0].page_content)
+
     document_query =  """Extract the values of PO Number, Ship To address, Ship Via method, PC Number and part Numbers.
-    Please note that PO can also be mentioned as Purchase Contract as well, in that case consider PO number as the number mentioned.
     PC numbers will be mentioned as PC Number. Please note that some documents 
-    might not have a PC Number mentioned, in that case assign value as PC number not Found.
+    might not have a PC Number mentioned, in that case assign value as 'PC number not Found'.
     Please note that some documents might not have The Ship Via method mention, 
-    in that  case assign value as 'Ship Via/Freight Method Not Found'.
-    Please note that if in the part/description table, a number/alphanumeric number is mentioned after the term Zebra, consider ONLY that to be the actual Part Number otherwise consider Part Numbers to be only alphabets/alphanumeric without any spaces inside the Item/description table only.
-    Please note that multiple Part Numbers might also be mentioned in the table with column as 'SCHEDULE OF SUPPLIES AND SERVICES, in that case extract only those the part numbers.
-    Special cases to extract Part Numbers/Item number: Part numbers are a single alphanumeric number with hyphens in between and no spaces. 
-                                                     Sometimes the part number maybe printed on the pdf in seperate lines with a hyphen-, in those cases, the entire alphanumeric number is the Part number.
+    in that  case assign value as Freight Method Not Found.
+    Please note that if in the part/description table, a number/alphanumeric number is mentioned after the term 'Zebra', consider ONLY that number to be the actual Part Number,excluding any other number.
+    Special cases to extract Part Numbers/Item number:
+    For example, the actual part number is ABC-2HG3-IOX but the part of the string can be present as ABC-2HG3- in the first line and rest IOX in the immediate next line.
+       In that case, consider the whole string as the part number without separating it as two part numbers. 
+    In case, no part numbers is found as per requirement, assign value as 'Part Number Not found'
     """+ document[0].page_content
 
     prompt_format = prompt.format_prompt(question = document_query)
@@ -56,21 +58,24 @@ def document_load_and_parse(temp_pdf_path,prompt,client):
     logging.info(parsed)
     return parsed      
 
-def validateParsedValuesWithDatabase(username,password,dsn,parsed):
+def validate_parsed_values_with_database(username,password,dsn,parsed):
     
     logging.info("Document Validation starts.......")
     global connection
     remarks = []
+    global flag 
+    flag = False
     try:        
-        cx_Oracle.init_oracle_client(lib_dir= r"C:\\Oracle19c-64bit\\product\\client_1\\bin\\")
-        connection = cx_Oracle.connect(user=username, password=password, dsn=dsn)
-        logging.info(f"\nConnected successfully!")
+        if not flag:
+            cx_Oracle.init_oracle_client(lib_dir= r"C:\\Oracle19c-64bit\\product\\client_1\\bin\\")
+            connection = cx_Oracle.connect(user=username, password=password, dsn=dsn)
+            logging.info(f"\nConnected successfully!")
+            flag = True
 
     except cx_Oracle.DatabaseError as e:
         logging.info(f"Error connecting to the database: {e}")
-        connection.close()
 
-    logging.info(f"Connected to Oracle Database version: {connection.version}")
+
     cursor = connection.cursor()
     po_no_validate = f"SELECT X_ZEB_PURCHASE_ORDNUM FROM siebel.S_ORDER where X_ZEB_PURCHASE_ORDNUM= :value"
     freight_account_number_validate = "SELECT X_CUST_FREIGHT_ACCOUNT FROM siebel.S_ORDER WHERE X_CUST_FREIGHT_ACCOUNT= :value"
@@ -98,12 +103,12 @@ def validateParsedValuesWithDatabase(username,password,dsn,parsed):
         print(f"PO number {parsed.po_number} already exists.")
 
 
-    if "BESTWAY" in parsed.freight_acc_no or "PREPAY & ADD" in parsed.freight_acc_no:
+    if "BESTWAY" in parsed.freight_acc_no or "Prepay & Add" in parsed.freight_acc_no:
         print(f"No Validation needed for Freight account number: {parsed.freight_acc_no}")
         remarks.append("No Validation needed for Freight account number")
     elif parsed.freight_acc_no == 'Ship Via/Freight Method Not Found':
         print(f"Freight Account Number not found")
-        remarks.append("Freight Account Number not found") #freight acc number not found [Prepay & add, then ignore Freight validation]
+        remarks.append("Freight Account Number not found") 
     else:
         print(f"Freight Account Number is: {parsed.freight_acc_no}")
 
@@ -127,22 +132,30 @@ def validateParsedValuesWithDatabase(username,password,dsn,parsed):
     return remarks
 
 
-def create_excel_file(file_path,parsed,remarks):
+def create_excel_file(blob_service_client,container_name,upload_excel_blob_name,parsed,remarks):
     logging.info("Creating Excel File with the Validation Errors Info.....")
     dict_data1 = parsed.dict()
     print(dict_data1)
     combined_part_numbers=','.join(parsed.part_numbers)
     combined_remarks = ','.join(remarks)
-
-    if os.path.exists(file_path):
-        print(f"The file {file_path} already exists.")
-        df = pd.read_excel(file_path)
+    blob_client = blob_service_client.get_blob_client(container_name,upload_excel_blob_name)
+    logging.info(f"blob client :{blob_client}")
+    if blob_client.exists():
+        # Download the blob content
+        blob_content = blob_client.download_blob()
+        logging.info(f"The file {blob_content} already exists.")
+        df = pd.read_excel(blob_content.content_as_bytes())
         new_row = dict_data1.copy()
         new_row['Remarks'] = combined_remarks
         new_row['Sl. No.'] = len(df) + 1
         df = df._append(new_row, ignore_index=True)
-        df.to_excel(file_path, index=False)
-        print(f"The DataFrame has been updated to {file_path}. ")
+
+        # Save the modified data to a new Excel file
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='Sheet1', index=False)
+        xlsx_data = output.getvalue()
+        logging.info(f"The DataFrame has been updated to {xlsx_data}. ")
 
     else:
         data1 = pd.DataFrame(dict_data1)
@@ -151,78 +164,86 @@ def create_excel_file(file_path,parsed,remarks):
         data1.insert(0, 'Sl. No.', range(1, 1 + len(data1)))
         print("printing data1..... ")
         print(data1)
-        data1.to_excel(file_path, index=False)
-        print(f"New DataFrame has been saved to {file_path}.")
+        #save the dataframe to excelfile
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            data1.to_excel(writer, sheet_name='Sheet1', index=False)
 
-# def create_excel_file(file_path,parsed):
-#     logging.info("Creating Excel File with the Validation Errors Info.....")
-#     dict_data1 = parsed.dict()
-#     print(dict_data1)
-#     combined_remarks = ','.join(remarks)
-
-#     if os.path.exists(file_path):
-#         print(f"The file {file_path} already exists.")
-#         df = pd.read_excel(open(file_path, 'rb'))
-#         df = df._append(dict_data1, ignore_index=True)
-#         df['Remarks'] = combined_remarks
-#         df.to_excel(file_path, index=False)
-#         print(f"The DataFrame has been updated to {file_path}.")
-
-#     else:
-#         data1 = pd.DataFrame(dict_data1)
-#         data1['Remarks'] = combined_remarks
-#         data1.insert(0, 'Sl. No.', range(1, 1 + len(data1)))
-#         print("printing data1.....")
-#         print(data1)
-#         data1.to_excel(file_path, index=False)
-#         print(f"New DataFrame has been saved to {file_path}.")
-
+        xlsx_data = output.getvalue()
+        logging.info(f"New DataFrame has been saved to {xlsx_data}.")
     
-def upload_excel_blob(account_name, account_key, container_name, local_file_path, upload_excel_blob_name):
-    logging.info("Uploading error Excel to the Blob.....")
-    try:
-        # Create a BlobServiceClient using your storage account credentials
-        blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
-
-        # Get a container client
-        container_client = blob_service_client.get_container_client(container_name)
-
+    return xlsx_data
         
 
-        # Upload the local Excel file to the blob
-        with open(local_file_path, "rb") as data:
-            blob_client = container_client.upload_blob(name=upload_excel_blob_name, data=data, overwrite=True)
-            print(f"Uploaded {upload_excel_blob_name} successfully!")
+def upload_excel_blob(blob_service_client,container_name, xlsx_data, upload_excel_blob_name):
+    logging.info("Uploading error Excel to the Blob.....")
+    try:
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=upload_excel_blob_name)
+        blob_client.upload_blob(xlsx_data, overwrite=True)
+        if blob_client.exists:
+            logging.info(f"Uploaded {upload_excel_blob_name} successfully!")
+        else:
+            logging.info(f"Error uploading {upload_excel_blob_name}")
 
     except Exception as e:
-        print(f"Error uploading {upload_excel_blob_name}: {e}")
+        logging.info(f"Error uploading {upload_excel_blob_name}: {e}")
 
+def send_alert_mail_using_sendgrid(API,upload_excel_blob_name,xlsx_data):
+
+    message = Mail(
+        from_email = os.environ.get("FROM"),
+        to_emails = os.environ.get("TO"),
+        subject = "ZEBRA OM GENAI PO Parser Alert Email",
+        html_content = "Alert Mail with Excel Sheet is sent Successfully!" 
+    )
+    
+    encoded_file = base64.b64encode(xlsx_data).decode()
+
+    attachedFile = Attachment(
+        FileContent(encoded_file),
+        FileName(upload_excel_blob_name),
+        FileType('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
+        Disposition('attachment')
+    )
+    message.attachment = attachedFile
+    try:
+        sg = SendGridAPIClient(api_key=API)
+        print("sendgrid")
+        response = sg.send(message)
+        logging.info(f"Mail Response: {response}")
+        logging.info(f"Email sent! Status code: {response.status_code}")
+        logging.info(response.body)
+        logging.info(response.headers)
+    except Exception as e:
+        print(f"Exception{e}")
+    logging.info("email sent status")
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="po-container/{name}.pdf",connection="doc1storageacc_STORAGE") 
-def blob_trigger1(myblob: func.InputStream):
-    print("entering logs")
+@app.blob_trigger(arg_name="myblob", path="po-container/{name}.pdf",
+                               connection="AzureWebJobsStorage") 
+def BlobTrigger1(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
+                f"Blob Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
-    # Replace with your actual connection string and blob/container names
-    connection_string = os.environ.get("CONNECTION_STRING")
-    container_name = "po-container"
+    
+    connection_string = os.environ.get("STORAGE_ACCOUNT_CONNECTION_STRING")
+    container_name = os.environ.get("CONTAINER_NAME")
     blob_name = myblob.name.split("/")[1]
     storage_acc_name = os.environ.get("AZURE_STORAGE_ACCOUNT")
-    azure_account_key = os.environ.get("AZURE_ACCOUNT_KEY")
+    storage_account_key = os.environ.get("AZURE_STORAGE_ACCOUNT_KEY")
     username = config.username
     password = config.password
-    local_excel_file_path = r"C:\Users\TR8361\my_env\sample_excel.xlsx"
-    upload_excel_blob_name = "sample_excel.xlsx"
+    upload_excel_blob_name = "status_excel.xlsx"
+    sendgrid_api_key = os.environ.get("SENDGRID_API_KEY")
+    
+
 
     dsn = cx_Oracle.makedsn(
         host=config.host,
         port=config.port,
         service_name = config.service_name
     )
-
 
     prompt = ChatPromptTemplate(
     messages=[
@@ -269,17 +290,15 @@ def blob_trigger1(myblob: func.InputStream):
         with open(temp_pdf_path, 'wb') as temp_file:
             temp_file.write(pdf_bytesio.getbuffer())
 
+        
         logging.info("Document Parsing starts......")
         parsed_return_value  = document_load_and_parse(temp_pdf_path,prompt,client)
-        remarks_list = validateParsedValuesWithDatabase(username,password,dsn,parsed_return_value)
-        create_excel_file(local_excel_file_path,parsed_return_value,remarks_list)
-        upload_excel_blob(storage_acc_name, azure_account_key, container_name, local_excel_file_path, upload_excel_blob_name)
-
+        remarks_list = validate_parsed_values_with_database(username,password,dsn,parsed_return_value)
+        xlsx_data = create_excel_file(blob_service_client,container_name,upload_excel_blob_name,parsed_return_value,remarks_list)
+        logging.info("uploading excel to blb container starts.....")
+        upload_excel_blob(blob_service_client, container_name, xlsx_data, upload_excel_blob_name)
+        logging.info("sending mail alert ....")
+        send_alert_mail_using_sendgrid(sendgrid_api_key,upload_excel_blob_name,xlsx_data)
     else:
         logging(f"Blob '{blob_name}' does not exist.")
 
-    
-
-
-
-    
